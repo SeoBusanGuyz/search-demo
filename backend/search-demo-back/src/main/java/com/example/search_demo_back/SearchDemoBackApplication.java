38b1e3c316d72bcf1e47142ad7fa99d76e2e50a8
sumin kim <cm14121453@gmail.com>
2025-07-16 23:43:40 +0900

    chore: Spring Boot(Gradle) 백엔드 초기 생성

---
+++ backend/search-demo-back/src/main/java/com/example/search_demo_back/SearchDemoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.search_demo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchDemoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchDemoBackApplication.class, args);
+	}
+
+}
---
+++ backend/search-demo-back/src/test/java/com/example/search_demo_back/SearchDemoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.search_demo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchDemoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
